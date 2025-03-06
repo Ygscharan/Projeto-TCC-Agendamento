329cab5de49aed905f8fe165867ae6e34e812b98
Ygscharan <153247471+Ygscharan@users.noreply.github.com>
2025-03-05 22:09:10 -0300

    Alterando fornecedor para receber as credenciais

--- server/controllers/fornecedorController.js
+++ server/controllers/fornecedorController.js
@@ -11,10 +11,11 @@ const getFornecedores = async (req, res) => {
 };
 
 // Controlador para criar um novo fornecedor
+// precisa ainda colocar o email e a validação desses campos
 const addFornecedor = async (req, res) => {
-    const { nome, email, telefone } = req.body;
+    const {nome, cnpj, endereco, telefone } = req.body;
     try {
-        const fornecedor = { nome, email, telefone };
+        const fornecedor = { nome, cnpj, endereco, telefone };
         const result = await Fornecedor.createFornecedor(fornecedor);
         res.status(201).json({ message: 'Fornecedor criado com sucesso', result });
     } catch (err) {
